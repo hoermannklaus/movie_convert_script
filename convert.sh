6973aecb193b62324989568baf7677adab06249f
Klaus Hörmann-Engl <hoermann.klaus@gmail.com>
2016-10-10 21:50:35 +0200

    Added nice console output, added kind of dry run function

--- convert.sh
+++ convert.sh
@@ -5,25 +5,39 @@
 # Description: Convert XVID movies to H264 movies (AVC)
 #---------------------
 
-# TODO: Add "dry-run" option!!!!
+# Exit script when command fails
+set -o errexit
 
 # TODO: Add function to combine 2 files into a single movie using "ffmpeg"
 # ffmpeg -f concat -i <(for f in ./*.m4v; do echo "file '$PWD/$f'"; done) -c copy movie_file_path.m4v
 
 # TODO: Add configuration:
 # - Single file given (input => output)
-#- 
 
 
 # General variables
-VERSION=0.3
-MOVIE_COUNTER=0
-MAX_MOVIE_AMOUNT=5
+DRY="y"
+VERSION=0.4
+MOVIE_COUNTER=1
+MOVIE_PART_COUNTER=0
+MAX_MOVIE_AMOUNT=3
+
 
 # Movie paths
 MOVIE_PATH_OLD="/mnt/NAS_video/Movies Convert/De/"
 MOVIE_PATH_NEW="/mnt/NAS_video/Movies/De/"
-MOVIE_OBSOLETE="/mnt/NAS_video/Moveies OBSOLETE/De/"
+MOVIE_OBSOLETE="/mnt/NAS_video/Movies OBSOLETE/De/"
+
+
+# Execute function, respects dry run option
+execute(){
+	echo "COMMAND: ${@}"
+	if [ "$DRY" = "y" ]; then
+		return 0;
+	fi
+	eval "$@"
+}
+
 
 clear
 
@@ -33,8 +47,13 @@ echo "Convert XVID, DIVX, ... to H264 (AVC)\n"
 # Go through all movie dirs in the root path
 for movie_dir_path in "$MOVIE_PATH_OLD"*; do
 
+	MOVIE_PART_COUNTER=0
+
 	# Go through all movie files in the movie path
 	for movie_file_path in "$movie_dir_path"/*; do
+
+		# Raise movie part counter
+		MOVIE_PART_COUNTER=$((MOVIE_PART_COUNTER+1))
 
 		# Set movie_dir and movie_file
 		movie_dir=`basename "$movie_dir_path"`
@@ -46,36 +65,37 @@ for movie_dir_path in "$MOVIE_PATH_OLD"*; do
 
 		# Check if codec is "XVID"
 		if [ "$codec" = "XVID" ]; then
+			
+			
+			echo "\n\n\n---------------- MOVIE $MOVIE_COUNTER-$MOVIE_PART_COUNTER ----------------"
 
-			# COMMAND: Create new directory
-			mkdir -p "$MOVIE_PATH_NEW$movie_dir"
+			echo "  Create new movie directory"
+			execute "mkdir -p \"$MOVIE_PATH_NEW$movie_dir\""
 
-			# Convert old file into new file
-			echo "Codec: "$codec" => Convert"
-			echo "Old movie_file: "$movie_file_path
-			echo "New movie_file: "$new_movie_file_path
+			echo "  Used codec of old movie file: "$codec" => Convert to AVC"
+			echo "  Old movie file: "$movie_file_path
+			echo "  New movie file: "$new_movie_file_path
 		
-			# COMMAND: Convert to H264
-			HandBrakeCLI -i "$movie_file_path" -o "$new_movie_file_path" --preset="Normal"
+			echo "  Convert XVID to AVC using Handbrake CLI"
+			execute "HandBrakeCLI -i \"$movie_file_path\" -o \"$new_movie_file_path\" --preset=\"Normal\""
 
-			# COMMAND: Rename new movie file to m4v
-			mv "$new_movie_file_path" "$MOVIE_PATH_NEW$movie_dir""/""`basename "$new_movie_file_path" .avi`.m4v"
+			echo "  Rename new movie extension from avi to m4v"
+			execute "mv \"$new_movie_file_path\" \"$MOVIE_PATH_NEW$movie_dir\"\"/\"\"`basename \"$new_movie_file_path\" .avi`.m4v\""
 
-			echo ""
 		fi
 
 	done
 
-	# COMMAND: Move old movie folder to obsolete folder
-	mv "$movie_dir_path" "$MOVIE_OBSOLETE"
+	echo "  Move old movie folder to obsolete folder"
+	execute "mv \"$movie_dir_path\" \"$MOVIE_OBSOLETE\""
 
-	# Raise counter
-	MOVIE_COUNTER=$((MOVIE_COUNTER+1))
-	
 	# Break when movie counter exceeds max movie amount
 	if [ $MOVIE_COUNTER -eq $MAX_MOVIE_AMOUNT ]; then
 		break
 	fi
+
+	# Raise counter
+	MOVIE_COUNTER=$((MOVIE_COUNTER+1))
 	
 done
 
