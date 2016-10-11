7599b6c2d2a796e92b39630ac057c21cd5066d48
Klaus Hörmann-Engl <hoermann.klaus@gmail.com>
2016-10-11 23:02:48 +0200

    Added ffmpeg to merge 2 files

--- convert.sh
+++ convert.sh
@@ -16,22 +16,25 @@ set -o errexit
 
 
 # General variables
-DRY="y"
+DRY="n"
 VERSION=0.4
 MOVIE_COUNTER=1
 MOVIE_PART_COUNTER=0
-MAX_MOVIE_AMOUNT=3
+MAX_MOVIE_AMOUNT=1
+
+# Convertion settings
+HANDBRAKE_PRESET="Normal"
 
 
 # Movie paths
-MOVIE_PATH_OLD="/mnt/NAS_video/Movies Convert/De/"
+MOVIE_PATH_OLD="/mnt/NAS_video/Movies Convert/Test/"
 MOVIE_PATH_NEW="/mnt/NAS_video/Movies/De/"
 MOVIE_OBSOLETE="/mnt/NAS_video/Movies OBSOLETE/De/"
 
 
 # Execute function, respects dry run option
 execute(){
-	echo "COMMAND: ${@}"
+	echo " COMMAND: ${@}"
 	if [ "$DRY" = "y" ]; then
 		return 0;
 	fi
@@ -48,6 +51,10 @@ echo "Convert XVID, DIVX, ... to H264 (AVC)\n"
 for movie_dir_path in "$MOVIE_PATH_OLD"*; do
 
 	MOVIE_PART_COUNTER=0
+	movie_dir=`basename "$movie_dir_path"`
+	new_movie_dir_path=$MOVIE_PATH_NEW$movie_dir
+
+	echo "\n\n\n---------------- MOVIE $MOVIE_COUNTER: $movie_dir ----------------"
 
 	# Go through all movie files in the movie path
 	for movie_file_path in "$movie_dir_path"/*; do
@@ -56,7 +63,7 @@ for movie_dir_path in "$MOVIE_PATH_OLD"*; do
 		MOVIE_PART_COUNTER=$((MOVIE_PART_COUNTER+1))
 
 		# Set movie_dir and movie_file
-		movie_dir=`basename "$movie_dir_path"`
+		
 		movie_file=`basename "$movie_file_path"`
 		new_movie_file_path=$MOVIE_PATH_NEW$movie_dir"/"$movie_file
 
@@ -66,8 +73,7 @@ for movie_dir_path in "$MOVIE_PATH_OLD"*; do
 		# Check if codec is "XVID"
 		if [ "$codec" = "XVID" ]; then
 			
-			
-			echo "\n\n\n---------------- MOVIE $MOVIE_COUNTER-$MOVIE_PART_COUNTER ----------------"
+			echo "\nFILE $MOVIE_PART_COUNTER:"
 
 			echo "  Create new movie directory"
 			execute "mkdir -p \"$MOVIE_PATH_NEW$movie_dir\""
@@ -77,7 +83,7 @@ for movie_dir_path in "$MOVIE_PATH_OLD"*; do
 			echo "  New movie file: "$new_movie_file_path
 		
 			echo "  Convert XVID to AVC using Handbrake CLI"
-			execute "HandBrakeCLI -i \"$movie_file_path\" -o \"$new_movie_file_path\" --preset=\"Normal\""
+			execute "HandBrakeCLI -i \"$movie_file_path\" -o \"$new_movie_file_path\" --preset=\"$HANDBRAKE_PRESET\""
 
 			echo "  Rename new movie extension from avi to m4v"
 			execute "mv \"$new_movie_file_path\" \"$MOVIE_PATH_NEW$movie_dir\"\"/\"\"`basename \"$new_movie_file_path\" .avi`.m4v\""
@@ -86,9 +92,19 @@ for movie_dir_path in "$MOVIE_PATH_OLD"*; do
 
 	done
 
-	echo "  Move old movie folder to obsolete folder"
-	execute "mv \"$movie_dir_path\" \"$MOVIE_OBSOLETE\""
+	echo "\nMOVIE:"
+	#echo " Move old movie folder to obsolete folder"
+	#execute "mv \"$movie_dir_path\" \"$MOVIE_OBSOLETE\""
 
+	# When the movie part counter > 1
+	if [ "$MOVIE_PART_COUNTER" -gt 1 ]; then
+		# TODO: This is not working "$f" is not working
+		echo "Use ffmpeg to make 1 file out of 2 files"
+		execute "ffmpeg -f concat -i <(for f in \"$new_movie_dir_path/\"*.m4v; do echo \"file '\"$f\"'\"; done) -c copy \"$new_movie_dir_path/$movie_dir.m4v\""
+	fi
+
+	# TODO: Delete the original 2 files "CD1 and CD2"
+	
 	# Break when movie counter exceeds max movie amount
 	if [ $MOVIE_COUNTER -eq $MAX_MOVIE_AMOUNT ]; then
 		break
@@ -98,4 +114,6 @@ for movie_dir_path in "$MOVIE_PATH_OLD"*; do
 	MOVIE_COUNTER=$((MOVIE_COUNTER+1))
 	
 done
+
+echo "\n\n\n"
 
